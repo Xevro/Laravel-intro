42bf7dd86a872ef54f8d4c42b61ae469463c4ca4
Louis D'Hont <louisdhont@MacBook-Pro-15-van-Louis.local>
2021-02-10 10:37:33 +0100

    Added simple db connection

--- app/routes/web.php
+++ app/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    $concerts = DB::table('concerts')->get();
+    dd($concerts);
     return view('welcome');
 });
